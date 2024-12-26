933d35e9d6256c5769fa7e5e79678b92b397f679
moicben <benjamain.georges@gmail.com>
2024-12-26 22:17:23 +0700

    v2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Utiliser l'exportation statique
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    const landingData = require('./landing.json');
+    const paths = {
+      ...defaultPathMap,
+      '/[category]/[subcategory]': { page: '/[category]/[subcategory]' },
+      '/[category]/[subcategory]/[landing]': { page: '/[category]/[subcategory]/[landing]' },
+    };
+
+    // Add paths for each dynamic route
+    landingData.pages.forEach(page => {
+      paths[`/${page.category}/${page.subCategory}/${page.slug}`] = {
+        page: '/[category]/[subcategory]/[landing]',
+        query: { category: page.category, subcategory: page.subCategory, landing: page.slug },
+      };
+    });
+
+    return paths;
+  },
 };
 
 module.exports = nextConfig;
